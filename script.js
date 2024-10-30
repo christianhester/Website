85e84811a14003f121d8022e582005cb855a2da5
christianhester <132182390+christianhester@users.noreply.github.com>
2024-10-30 14:14:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,27 @@
-$("section").hover(function() {
-    $(this).addClass("hover");
-    $(this).siblings().addClass("unhover");
-  }, function() {
-    $(this).removeClass("hover");
-    $(this).siblings().removeClass("unhover");
-  }
-);
+const container = document.getElementById('draggableContainer');
+const titleBar = container.querySelector('.title-bar');
 
-$("section").click(function(){
-  $(this).addClass("click");
-  $(this).children('div').delay(300).fadeIn(150);
-  $(this).siblings().addClass("unclick");
-  $(this).siblings().children("p").fadeOut(50);
+let offsetX = 0, offsetY = 0;
+let isDragging = false;
+
+titleBar.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    offsetX = e.clientX - container.getBoundingClientRect().left;
+    offsetY = e.clientY - container.getBoundingClientRect().top;
 });
 
-$(".x").click(function(event){
-  event.stopPropagation();
-  $("section").removeClass("click unclick");
-  $("section").siblings().children('div').fadeOut(150);
-  $("section").siblings().children('p').fadeIn(50);
-});
-
-$(document).keydown(function (e) {
-    switch (e.which) {
-    case 27:
-        reset();
-        break;
-    case 49:
-        open(1);
-        break;
-    case 50:
-        open(2);
-        break;
-    case 51:
-        open(3);
-        break;
-    case 52:
-        open(4);
-        break;
-    default:
-        return;
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        container.style.left = `${e.clientX - offsetX}px`;
+        container.style.top = `${e.clientY - offsetY}px`;
     }
-    e.preventDefault();
 });
 
-function open(nr) {
-  var t = $("section:nth-child("+nr+")");
-  reset();
-  t.toggleClass("click");
-  t.children('div').delay(300).fadeToggle(300);
-  t.siblings().toggleClass("unclick");
-  t.siblings().children("p").fadeToggle(50);
-}
+document.addEventListener('mouseup', () => {
+    isDragging = false;
+});
 
-function reset() {
-  $("section").removeClass("click unclick");
-  $("section").siblings().children('div').fadeOut(150);
-  $("section").siblings().children('p').fadeIn(50);
+// Optional: Close button functionality
+function closeWindow() {
+    container.style.display = 'none';
 }
